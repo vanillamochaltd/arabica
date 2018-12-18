03b2e7fd295d0d736d5d886f6fa721ccfcaee382
clcuenca <cuenca.cl@gmail.com>
2018-12-18 00:33:49 -0800

    add arabica test cases

---
+++ arabica_test.hpp
@@ -0,0 +1,36 @@
+// ----------------------------------------
+// @file: arabica_test.hpp
+// @author: carlos l cuenca
+// @description: test functions for arabica
+
+#ifndef ARABICA_TEST
+#define ARABICA_TEST
+
+#include <iostream>
+#include <cinttypes>
+#include "arabica.hpp"
+
+// ------------------------------------------------
+// @namespace: arabica test
+// @author: carlos l cuenca
+// @description: declaration for arabica test cases
+
+namespace ArabicaTest{
+
+
+	// ----------------
+	// Type Definitions
+
+	typedef uint32_t StatusCode;
+
+	// --------------
+	// Test Functions
+
+	template<typename Type>
+	StatusCode TestArray ();
+
+
+
+};
+
+#endif
