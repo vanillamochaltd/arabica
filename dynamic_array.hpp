5e576ec9ced316ac29615e12c8bc573489a6194c
clcuenca <cuenca.cl@gmail.com>
2018-12-20 18:25:21 -0800

    add l inear and exponential growth functions to dynamic array

--- dynamic_array.hpp
+++ dynamic_array.hpp
@@ -18,8 +18,14 @@ class Arabica::DynamicArray: public Arabica::Array<Type, Index>{
 
 private:
 
+	// -------------------------
+	// @private_method
+	// @mutator: GrowPast, SetTo
+
 	inline void GrowPast(Index);
 	inline void SetTo(Arabica::Array<Type, Index> const&);
+	static Type* GrowLinear(Index&, Index);
+	static Type* GrowExponential(Index&, Index);
 
 public:
 
@@ -34,7 +40,7 @@ public:
 	// @public_method:
 	// @mutator: ChangeGrowth
 	
-	inline static void ChangeGrowth(float);
+	inline static void SetGrowth(float);
 	void SetTo(Type const*, Index);	
 
 	// --------------------
@@ -63,6 +69,7 @@ public:
 
 template<typename Type, typename Index>
 Arabica::DynamicArray<Type, Index>::DynamicArray(Index length): Arabica::Array<Type, Index>(length){
+
 
 }
 
@@ -101,9 +108,75 @@ Arabica::DynamicArray<Type, Index>::~DynamicArray(){}
 // @description: Changes the growth rate of dynamic arrays
 
 template<typename Type, typename Index>
-void Arabica::DynamicArray<Type, Index>::ChangeGrowth(float growth_rate){
+void Arabica::DynamicArray<Type, Index>::SetGrowth(float growth_rate){
 
 	Arabica::Dynamic_Array_Growth = growth_rate;
+
+}
+
+// -----------------------------------------------------------
+// @method: GrowPast
+// @class: dynamic array
+// @author: carlos l cuenca
+// @parameters: length
+// @description: grows the internal block past the given index
+// by default, the block growth is linear
+
+template<typename Type, typename Index>
+void Arabica::DynamicArray<Type, Index>::GrowPast(Index size){
+
+	Arabica::Array<Type, Index> buffer(*this);
+	
+	if(this->size_) delete[] this->array_;
+
+	else this->size_ = 0x1;
+
+	this->array_ = GrowLinear(this->size_, size);
+
+	for(Index index = 0x0; index < this->length_; index++)
+		this->array_[index] = buffer[index];
+
+}
+
+// ------------------------------------------------------------------------------------
+// @method: GrowLinear
+// @class: dynamic array
+// @author: carlos l cuenca
+// @parameters: length
+// @description: calculates a linear value, allocates the space, and returns the pointer
+
+template<typename Type, typename Index>
+Type* Arabica::DynamicArray<Type, Index>::GrowLinear(Index& current_size, Index target_size){
+
+	while(current_size <= target_size) current_size *= Arabica::Dynamic_Array_Growth;
+
+	Type* array = new Type[current_size]();
+
+	return array;
+
+}
+
+// -------------------------------------------------------------------
+// @method: GrowExponential
+// @class: dynamic array
+// @author: carlos l cuenca
+// @parameters: length, target length
+// @description: calculates an exponential value, allocates the space,
+// and returns the pointer to the new block
+
+template<typename Type, typename Index>
+Type* Arabica::DynamicArray<Type, Index>::GrowExponential(Index& current_size, Index target_size){
+
+	float size      = current_size ;
+	float threshold = target_size  ;
+
+	while(size <= threshold) size *= Arabica::Dynamic_Array_Growth;
+
+	current_size = size;
+
+	Type* array = new Type[current_size]();
+
+	return array;
 
 }
 
